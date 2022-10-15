33d9ea2dc4f8fc3ea87174c2eb57d26ddf547b17
Albert <alberto.suarez8@outlook.com>
2022-10-15 14:18:18 -0400

    Refactored starter code and added a modal to the generate password button. README Credits updated.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,36 @@
-// Assignment code here
+// Get the modal 
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector("#generate");
+
+// Get the <span> element that closes the modal 
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = closeModal;
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password Generator Starter Code
+
+## Credits
+
+- [Starter code provided by UCF Coding Bootcamp](https://github.com/coding-boot-camp/friendly-parakeet)
+- [Modal code provided by W3Schools](https://www.w3schools.com/howto/howto_css_modals.asp)
