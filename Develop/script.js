633b10a612084d3f956cbf3b697f090832098d15
Albert <alberto.suarez8@outlook.com>
2022-10-15 15:06:33 -0400

    Added range slider to popup modal. added more credits to the README file.

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,14 @@ var btn = document.querySelector("#generate");
 // Get the <span> element that closes the modal 
 var span = document.getElementsByClassName("close")[0];
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -14,7 +22,6 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = closeModal;
-
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ## Credits
 
 - [Starter code provided by UCF Coding Bootcamp](https://github.com/coding-boot-camp/friendly-parakeet)
-- [Modal code provided by W3Schools](https://www.w3schools.com/howto/howto_css_modals.asp)
+- W3Schools deems fair use of their copyrighted material when it is linked like the following. For more information please visit [W3Schools Terms of Service](https://www.w3schools.com/about/about_copyright.asp).
+    - [Modal code provided by W3Schools](https://www.w3schools.com/howto/howto_css_modals.asp)
+    - [Range slider code provided by W3Schools](https://www.w3schools.com/howto/howto_js_rangeslider.asp)
