422d66c75bb7aae46bd77d65262e7858c8be004f
Albert <alberto.suarez8@outlook.com>
2022-10-16 20:50:14 -0400

    Added criteria variables and  function to return list of selected criteria

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,13 @@ var btn = document.querySelector("#generate");
 // Get the <span> element that closes the modal 
 var span = document.getElementsByClassName("close")[0];
 
+var apply = document.getElementById("apply-btn");
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
@@ -32,6 +39,23 @@ window.onclick = function(event) {
 
 function closeModal() {
   modal.style.display = "none";
+}
+
+function checkAppliedCriteria() {
+  var listofAppliedCriteria = [];
+  if (document.getElementById("uppercase").checked) {
+    listofAppliedCriteria.push(uppercase);
+  }
+  if (document.getElementById("lowercase").checked) {
+    listofAppliedCriteria.push(lowercase);
+  }
+  if (document.getElementById("numbers").checked) {
+    listofAppliedCriteria.push(numbers);
+  }
+  if (document.getElementById("special").checked) {
+    listofAppliedCriteria.push(special);
+  }
+  return listofAppliedCriteria;
 }
 
 // Write password to the #password input
